97156504827a0d2b98f32fa8e54b5e53e67466e5
nbogie <neillbogie@googlemail.com>
2021-09-14 22:33:13 +0100

    center page & limit page maxW

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import * as React from "react";
 import "./App.css";
 import Palettes from "./Palettes.js";
-import { VStack, ChakraProvider, Center, Heading } from "@chakra-ui/react";
+import { VStack, Center, Heading, Container } from "@chakra-ui/react";
 import SmallCenteredFooter from "./Footer";
 
 function App() {
   return (
-    <Center>
-      <VStack>
-        <Heading>Nice Colours, Quicker!</Heading>
-        <Palettes />
-        <SmallCenteredFooter />
-      </VStack>
-    </Center>
+    <Container maxW={1000}>
+      <Center>
+        <VStack>
+          <Heading>Nice Colours, Quicker!</Heading>
+          <Palettes />
+          <SmallCenteredFooter />
+        </VStack>
+      </Center>
+    </Container>
   );
 }
 
