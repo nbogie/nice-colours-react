8beae2e1e58da61eb9fc538205783bb23d79b52d
nbogie <neillbogie@googlemail.com>
2024-05-26 10:46:51 +0100

    remove web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
 import { themeOverride } from "./themeOverride";
 
 const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
@@ -17,8 +16,3 @@ root.render(
         </ChakraProvider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
