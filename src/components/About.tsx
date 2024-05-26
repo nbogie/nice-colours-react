da40bab935905c24dad81661c14ccb8a6e3b50c8
nbogie <neillbogie@googlemail.com>
2024-05-26 13:25:12 +0100

    add kgolid chromotome to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,18 +2,30 @@ import { Link } from "@chakra-ui/react";
 
 export function About() {
     return (
-        <p>
-            Top palettes from{" "}
-            <Link href="https://www.colourlovers.com/palettes" isExternal>
-                ColourLovers
-            </Link>
-            , via{" "}
-            <Link href="https://www.mattdesl.com/" isExternal>
-                Matt Deslauriers'
-            </Link>{" "}
-            <Link href="https://github.com/Jam3/nice-color-palettes" isExternal>
-                nice-color-palettes npm package.
-            </Link>
-        </p>
+        <>
+            <p>
+                Top palettes from{" "}
+                <Link href="https://www.colourlovers.com/palettes" isExternal>
+                    ColourLovers
+                </Link>
+                , via{" "}
+                <Link href="https://www.mattdesl.com/" isExternal>
+                    Matt Deslauriers'
+                </Link>{" "}
+                <Link
+                    href="https://github.com/Jam3/nice-color-palettes"
+                    isExternal
+                >
+                    nice-color-palettes npm package.
+                </Link>
+            </p>
+            <p>
+                Kjetil Golid's{" "}
+                <Link href="https://github.com/kgolid/chromotome" isExternal>
+                    Chromotome palettes
+                </Link>
+                .
+            </p>
+        </>
     );
 }
