0f817c3dd406f21791a4770432121c7e665fc2d7
nbogie <neillbogie@googlemail.com>
2024-05-26 11:39:34 +0100

    formatting

--- src/themeOverride.ts
+++ src/themeOverride.ts
@@ -1,18 +1,18 @@
 export const themeOverride = {
-  // colors: {
-  //   brand: {
-  //     100: "#f7fafc",
-  //     // ...
-  //     900: "#1a202c",
-  //   },
-  // },
-  components: {
-    Link: {
-      baseStyle: {
-        textDecorationLine: "underline",
-        // textDecorationThickness: "2px",
-        // textDecorationColor: "gray.400",
-      },
+    // colors: {
+    //   brand: {
+    //     100: "#f7fafc",
+    //     // ...
+    //     900: "#1a202c",
+    //   },
+    // },
+    components: {
+        Link: {
+            baseStyle: {
+                textDecorationLine: "underline",
+                // textDecorationThickness: "2px",
+                // textDecorationColor: "gray.400",
+            },
+        },
     },
-  },
 };
